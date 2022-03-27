a534d62748899b9d6661c9c4b4f8fc9521a6d6f8
Foo-Maker <Foo-Maker@Sven-Bast.eu>
2022-03-27 22:39:33 +0200

    added multi host support

--- ping.py
+++ ping.py
@@ -6,8 +6,8 @@
         host: The host we want to ping
         delay: The delay between the pings. Be careful. Dont set the delay to less. 
     """
-host = "pi.gringer" #the host to ping
-delay = 1 # delay between the pings
+host = "192.168.151.11" #the host to ping
+delay = 1 # delay between the pings in seconds
 lang = "de" # "DE" os "EN"
 port = 8000 # The http serverport to open the download from the logs 
 
@@ -26,6 +26,9 @@ start_time = time.strftime("%Y_%m_%d  %H:%M")
 count = 0
 i = False
 lang_index = "Will override with the language settings" 
+ips = "Will save the hosts from hosts.txt"
+current_directory = os.getcwd()
+dir = current_directory
 
 class log:
     def __init__(self, logfile):
@@ -39,9 +42,31 @@ class log:
     def __exit__(self, exc_type, exc_value, traceback):
         self.f.close()
 
-def eintrag(foo, boo):
+class hosts:
+    def __init__(self, file):
+        self.file = file
+        self.f = None
+    def __enter__(self):
+        self.f = open(self.file, "r")
+        return self
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.f.close()
+
+def eintrag(foo, boo, path):
+    os.chdir(path)
     with log(start_time + " " + boo + ".txt") as bar:
         bar.eintrag(foo)
+
+def ip():
+    global ips
+    foo = open('hosts.txt', 'r')
+    ips = ""
+    for line in foo:
+        ips = ips + line
+    ips = ips.split('\n')
+    res = [bar for bar in ips if bar.strip()]
+    ips = res
+    foo.close()
 
 def listen():
     global i
@@ -55,45 +80,56 @@ def timestamp():
     return time.asctime(foo)
 
 def mkdir(path):
-    os.mkdir(path)
-    
-def chdir(path):
-    os.chdir(path)
+    try:
+        os.chdir(current_directory + "/result")
+    except :
+        os.mkdir(current_directory + "/result")
+    os.chdir(current_directory + "/result")
+    try:
+        os.chdir(path)
+    except :
+        os.mkdir(path)
+
 
 def ping(host):
     foo = "ping -c 1 " + host #+ " | grep = | grep -v rtt"
-    time.sleep(delay)
     return os.popen(foo).read()
 
-def output(foo):
+def output(foo, host):
+    global dir
+    try:
+        os.chdir('/opt/ping')
+    except FileNotFoundError:
+        os.chdir(current_directory)
     bar = ping(host)
     bar = bar.strip()
+    mkdir(host)
+    os.chdir(current_directory + "/result/" + host)
     if lang_index in bar:
         bar = bar.splitlines()
         for line in bar:
             if lang_index in line:
                 print(f"{timestamp()} {line}")
-                eintrag(timestamp()+ "     " + line, "OK")
+                eintrag(timestamp()+ "     " + line, "OK", os.getcwd())
     elif 'erreichbar' in bar:
         print(f"\n\nLeider ging das in die Hose {timestamp()} {bar}\n\n")
-        eintrag("\n\n\n" + timestamp()+ "     " + bar, "Time_error")
+        eintrag("\n\n\n" + timestamp()+ "     " + bar, "Time_error", os.getcwd())
     else:
         print(f"\n\nUnbekannter Fehler um {timestamp()} Payload.: \n{bar} \n\n")
-        eintrag("\n\n\n" + timestamp() + "     " + bar, "Error")
+        eintrag("\n\n\n" + timestamp() + "     " + bar, "Error", os.getcwd())
+    os.chdir(current_directory)
 
 def workingdir(path):
     try:
         mkdir(path)
-        chdir(path)
+        os.chdir(path)
     except (FileExistsError):
-        chdir(path)
+        os.chdir(path)
 
 def webserv():
     server_object = HTTPServer(server_address=('', port), RequestHandlerClass=CGIHTTPRequestHandler)
     # Start the web server
     server_object.serve_forever()
-server = Thread(target=webserv)
-server.start()
 
 def set_lang():
     global lang_index
@@ -104,6 +140,13 @@ def set_lang():
     else:
         print(f"This language is not supported.")
 
+def run(host):
+    try:
+        output(count, host)
+    except ():
+        return 0
+    ip()
+
 def main():
     set_lang()
     os.system("clear")
@@ -111,14 +154,19 @@ def main():
     print(f"")
     print(f"Hit return to exit the program!")
     print(f"")
-    try:
-        count = 0
-        workingdir(host)
-        while i is False:
-            count += 1
-            output(count)
-    except ():
-        return 0
+    ip()
+    os.chdir(current_directory)
+    server = Thread(target=webserv)
+    server.start()
+    count = 0
+    while i is False:
+        time.sleep(delay)
+        count += 1
+        for foo in ips:
+            run(foo)
+            os.chdir(current_directory)
+        print(f"\n")
     os._exit(0)
 
 main()
+
